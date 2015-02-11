4de256635720e36a6a277d57a7574d9e351690f3
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-11 18:21:41 +0100

    basic

--- app/sentimentapi_v1.rb
+++ app/sentimentapi_v1.rb
@@ -4,22 +4,20 @@ require 'json'
 
 class SentimentApiV1 < Grape::API
     version 'v1', :using => :path, :vendor => '3scale'
-    #error_format :json
-    format :json
 
     resource :words do
         get ':word' do
-            {word: params[:word], sentiment:"unkown"}
+            {word: params[:word], sentiment:"unknown"}.to_json
         end
 
         post ':word' do
-            {word: params[:word], result: "thinking"}
+            {word: params[:word], result: "thinking"}.to_json
         end
     end
 
     resource :sentences do
         get ':sentence' do
-            {sentence: params[:sentence], result:"unkown"}
+            {sentence: params[:sentence], result:"unknown"}.to_json
         end
     end
 end
