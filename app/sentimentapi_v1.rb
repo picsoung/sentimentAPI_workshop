1927c1700ff1d9bca697668b01f32f254de50c35
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-12 17:22:34 +0100

    add reporting and format data

--- app/sentimentapi_v1.rb
+++ app/sentimentapi_v1.rb
@@ -6,30 +6,51 @@ require '3scale_client'
 class SentimentApiV1 < Grape::API
     version '1', :using => :header, :vendor => '3scale' #hackish
     format :json
-    
+
+    @@client = ThreeScale::Client.new(:provider_key => '53b85a912ba3596fd1bc7fdbaf07afa9' )
+
     helpers do
         def authenticate!
-            client = ThreeScale::Client.new(:provider_key => '53b85a912ba3596fd1bc7fdbaf07afa9' )
-            response = client.authrep( :user_key => params[:user_key] ,:usage => { :hits => 1 })
+            response = @@client.authrep( :user_key => params[:user_key] ,:usage => { :hits => 1 })
             error!('403 Unauthorized', 403) unless response.success?
+        end
+
+        def report!(method_name='hits', usage_value=1)
+            p params[:user_key]
+            response = @@client.report( {:user_key => params[:user_key],  :usage => {method_name => usage_value}})
+            error!('505 Reporting Error', 505) unless response.success?
         end
     end
 
     resource :words do
         get ':word' do
             authenticate!
-            {word: params[:word], sentiment:"unknown"}
+            report!('word_get', 1)
+            {
+                code: status,
+                status: "success",
+                data: {
+                    word: params[:word],
+                    sentiment:"unknown"
+                    }
+            }
         end
 
         post ':word' do
             authenticate!
-            {word: params[:word], result: "thinking"}
+            report!('word/post', 1)
+            {
+                code: status,
+                status: "success",
+                data: {word: params[:word], result: "thinking"}
+            }
         end
     end
 
     resource :sentences do
         get ':sentence' do
             authenticate!
+            report!('sentence/get', 1)
             {sentence: params[:sentence], result:"unknown"}
         end
     end
