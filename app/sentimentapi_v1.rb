b802c5064deacebfaf554b9c0db622f9372e3ddd
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-12 16:43:27 +0100

    add 3scale management

--- app/sentimentapi_v1.rb
+++ app/sentimentapi_v1.rb
@@ -1,23 +1,35 @@
 require 'rubygems'
 require 'grape'
 require 'json'
+require '3scale_client'
 
 class SentimentApiV1 < Grape::API
     version '1', :using => :header, :vendor => '3scale' #hackish
     format :json
+    
+    helpers do
+        def authenticate!
+            client = ThreeScale::Client.new(:provider_key => '53b85a912ba3596fd1bc7fdbaf07afa9' )
+            response = client.authrep( :user_key => params[:user_key] ,:usage => { :hits => 1 })
+            error!('403 Unauthorized', 403) unless response.success?
+        end
+    end
 
     resource :words do
         get ':word' do
+            authenticate!
             {word: params[:word], sentiment:"unknown"}
         end
 
         post ':word' do
+            authenticate!
             {word: params[:word], result: "thinking"}
         end
     end
 
     resource :sentences do
         get ':sentence' do
+            authenticate!
             {sentence: params[:sentence], result:"unknown"}
         end
     end
